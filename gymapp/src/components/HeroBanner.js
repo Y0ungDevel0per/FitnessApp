bb4a7ab5640501738bb2e0b379e3c8c0d9e3bee4
Akinsoji Richard <111376893+Y0ungDevel0per@users.noreply.github.com>
2022-11-17 12:01:42 +0100

    new push

--- gymapp/src/components/HeroBanner.js
+++ gymapp/src/components/HeroBanner.js
@@ -9,8 +9,8 @@ const HeroBanner = () => {
         mt: { lg: '212px', xs: '70px'},
         ml: { sm: '50px'}
     }} position="relative" p="20px" >
-        <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-            Fitness Club
+        <Typography color="#FF2625" fontWeight="bold" fontSize="50px">
+            Gymtastic
         </Typography>
         <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px'}}} mb="23px" mt="30px">
             Sweat, Smile <br /> and Repeat
