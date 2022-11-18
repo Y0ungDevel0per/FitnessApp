255fdd35c84ec63b4d76a0dd2c32e5d007dbfb1e
Akinsoji Richard <111376893+Y0ungDevel0per@users.noreply.github.com>
2022-11-18 10:06:14 +0100

    new push

--- gymapp/src/components/HorizontalScrollbar.js
+++ gymapp/src/components/HorizontalScrollbar.js
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
 import BodyPart  from './BodyPart';
-import ExerciseCard from './ExerciseCard';
+// import ExerciseCard from './ExerciseCard';
 
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
@@ -38,8 +38,7 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
           title={item.id || item}
           m="0 40px"
         >
-          {isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-          : <ExerciseCard exercise={item} />}
+          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
       ))}
     </ScrollMenu>
--- gymapp/src/pages/ExerciseDetail.js
+++ gymapp/src/pages/ExerciseDetail.js
@@ -5,13 +5,13 @@ import { Box } from '@mui/material';
 import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
-import SimilarExercises from '../components/SimilarExercises';
+// import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState({});
-  const [targetMuscleExercises, setTargetMuscleExercises] = useState({});
-  const [equipmentExercises, setEquipmentExercises] = useState({});
+  // const [targetMuscleExercises, setTargetMuscleExercises] = useState({});
+  // const [equipmentExercises, setEquipmentExercises] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
@@ -25,11 +25,11 @@ const ExerciseDetail = () => {
       const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
       setExerciseVideos(exerciseVideosData.contents);
 
-      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
-      setTargetMuscleExercises(targetMuscleExercisesData);
+      // const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      // setTargetMuscleExercises(targetMuscleExercisesData);
 
-      const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
-      setEquipmentExercises(equipmentExercisesData);
+      // const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      // setEquipmentExercises(equipmentExercisesData);
     }
 
     fetchExercisesData();
@@ -38,8 +38,8 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+      {/* <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} /> */}
+      {/* <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} /> */}
     </Box>
   )
 }
